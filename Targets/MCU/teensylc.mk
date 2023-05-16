816fc4e5626012272bfa8ede53ce731adb13ce70
Paul Schulze <pschulze@helindustries.com>
2023-05-16 07:23:20 +0200

    Add Teency LC upload target

--- Targets/MCU/teensylc.mk
+++ Targets/MCU/teensylc.mk
@@ -7,7 +7,11 @@ MCU = mkl26z64
 CPUARCH = cortex-m0plus
 CPU_CPPFLAGS =
 CPU_LDFLAGS = -larm_cortexM0l_math
-SERIAL_ID = "Teensy LC"
+SERIAL_ID = "Teensy\sLC"
 MCU_BOARD_RATE = 57600
 
 include $(MAKE_INC_PATH)/Targets/MCU/teensybase.mk
+
+upload_teensylc: upload_teensy
+
+.PHONY: upload_teensylc
