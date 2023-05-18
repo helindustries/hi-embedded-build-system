bd965d3a103ec5e64f4662b2a223bfb27ef8f62a
Paul Schulze <pschulze@helindustries.com>
2023-05-18 08:14:54 +0200

    Change modules to automatically use the src directory as module root if present, change the remaining target names

--- Modules.mk
+++ Modules.mk
@@ -1,20 +1,22 @@
 # Build module lib names and directories
 MODULES ?=
-MODULES_NAMES := $(foreach mod,$(MODULES), $(firstword $(subst :, ,$(mod))))
-MODULES_PATHS := $(foreach mod,$(MODULES), $(lastword $(subst :, ,$(mod))))
+$(foreach mod,$(MODULES),$(eval MODULES = $(patsubst $(mod),$(if $(wildcard $(abspath $(lastword $(subst :, ,$(mod))))/src),$(mod)/src,$(mod)),$(MODULES))))
+MODULES_NAMES := $(foreach mod,$(MODULES),$(firstword $(subst :, ,$(mod))))
+MODULES_PATHS := $(foreach mod,$(MODULES),$(lastword $(subst :, ,$(mod))))
 $(foreach mod,$(MODULES),$(eval MODULES_PATH_$(firstword $(subst :, ,$(mod))) = $(abspath $(lastword $(subst :, ,$(mod))))))
-$(foreach mod,$(MODULES),$(eval MODULES_FILES_$(firstword $(subst :, ,$(mod))) = $(foreach ext,c cpp S h, $(wildcard $(abspath $(lastword $(subst :, ,$(mod))))/*.$(ext) $(abspath $(lastword $(subst :, ,$(mod))))/**/*.$(ext)))))
-#MODULES_TARGETS = $(patsubst %,lib%-$(MCU).a.target, $(filter-out Core,$(MODULES_NAMES)))
-MODULES_TARGETS := $(patsubst %,lib%-$(MCU).a.target, $(MODULES_NAMES))
+$(foreach mod,$(MODULES),$(eval MODULES_FILES_$(firstword $(subst :, ,$(mod))) = $(foreach ext,c cpp S s h, $(wildcard $(abspath $(lastword $(subst :, ,$(mod))))/*.$(ext) $(abspath $(lastword $(subst :, ,$(mod))))/**/*.$(ext)))))
+$(foreach mod,$(MODULES),$(eval MODULES_FILES_$(firstword $(subst :, ,$(mod))) = $(filter-out Examples/%,$(filter-out examples/%,$(MODULES_FILES_$(firstword $(subst :, ,$(mod))))))))
+#MODULES_TARGETS = $(patsubst %,lib%-$(MCU).a.module, $(filter-out Core,$(MODULES_NAMES)))
+MODULES_TARGETS := $(patsubst %,lib%-$(MCU).a.module, $(MODULES_NAMES))
 MODULES_CFG_TARGETS := $(patsubst %,lib%-$(MCU).a.modulecfg, $(MODULES_NAMES))
 MODULES_LIBS := $(MODULES_NAMES:%=$(BUILD_DIR)/lib%-$(MCU).a)
 MODULES_BUILD_DIRS := $(MODULES_NAMES:%=$(BUILD_DIR)/%)
 
 ifeq ($(strip $(USE_ARDUINO_CORE)),yes)
 	CORE_TARGET := libCore-$(MCU).a.module
-CORE_BUILD_DIR := $(BUILD_DIR)/Core
-CORE_LIB := $(BUILD_DIR)/libCore-$(MCU).a
-MODULES_PATH_Core := $(CORE_PATH)
+	CORE_BUILD_DIR := $(BUILD_DIR)/Core
+	CORE_LIB := $(BUILD_DIR)/libCore-$(MCU).a
+	MODULES_PATH_Core := $(CORE_PATH)
 endif
 
 modules: $(MODULES_TARGETS) | silent
