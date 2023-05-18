32d37af24d15ed8febe92beb9831863477b498c8
Paul Schulze <pschulze@helindustries.com>
2023-05-18 08:20:09 +0200

    Add an additional layer of directories to scan for macOS bash, as it doesn't understand ** well

--- ModuleFlags.mk
+++ ModuleFlags.mk
@@ -1,8 +1,8 @@
 MODULE_BUILD_PATH := $(BUILD_DIR)/$(MODULE_NAME)
-MODULE_H_FILES := $(filter-out $(MODULE_PATH)/examples/%,$(wildcard $(MODULE_PATH)/*.h $(MODULE_PATH)/src/*.h $(MODULE_PATH)/src/**/*.h))
-MODULE_C_FILES := $(filter-out $(MODULE_PATH)/examples/%,$(wildcard $(MODULE_PATH)/*.c $(MODULE_PATH)/src/*.c $(MODULE_PATH)/src/**/*.c))
-MODULE_CPP_FILES := $(filter-out $(MODULE_PATH)/examples/%,$(wildcard $(MODULE_PATH)/*.cpp $(MODULE_PATH)/src/*.cpp $(MODULE_PATH)/src/**/*.cpp))
-MODULE_ASM_FILES := $(filter-out $(MODULE_PATH)/examples/%,$(wildcard $(MODULE_PATH)/*.S $(MODULE_PATH)/src/*.S $(MODULE_PATH)/src/**/*.S))
+MODULE_H_FILES := $(filter-out $(MODULE_PATH)/examples/%,$(filter-out $(MODULE_PATH)/Examples/%,$(wildcard $(MODULE_PATH)/*.h $(MODULE_PATH)/**/*.h $(MODULE_PATH)/**/**/*.h $(MODULE_PATH)/**/**/**/*.h)))
+MODULE_C_FILES := $(filter-out $(MODULE_PATH)/examples/%,$(filter-out $(MODULE_PATH)/Examples/%,$(wildcard $(MODULE_PATH)/*.c $(MODULE_PATH)/**/*.c $(MODULE_PATH)/**/**/*.c $(MODULE_PATH)/**/**/**/*.c)))
+MODULE_CPP_FILES := $(filter-out $(MODULE_PATH)/examples/%,$(filter-out $(MODULE_PATH)/Examples/%,$(wildcard $(MODULE_PATH)/*.cpp $(MODULE_PATH)/**/*.cpp $(MODULE_PATH)/**/**/*.cpp $(MODULE_PATH)/**/**/**/*.cpp)))
+MODULE_ASM_FILES := $(filter-out $(MODULE_PATH)/examples/%,$(filter-out $(MODULE_PATH)/Examples/%,$(wildcard $(MODULE_PATH)/*.S $(MODULE_PATH)/*.s $(MODULE_PATH)/**/*.S $(MODULE_PATH)/**/**/*.S $(MODULE_PATH)/**/**/**/*.S $(MODULE_PATH)/**/*.s $(MODULE_PATH)/**/**/*.s $(MODULE_PATH)/**/**/**/*.s)))
 
 ifeq ($(strip $(MODULE_NAME)),Core)
 	ifneq ($(strip $(USE_ARDUINO_MAIN)),yes)
