499ac48e9f06d4b4966a0a3e5ea1cd1902e1ef66
Paul Schulze <pschulze@helindustries.com>
2023-06-06 00:46:15 +0200

    Add a build-step to run shellcheck to verify shell scripts

---
+++ Shell.mk
@@ -0,0 +1,16 @@
+SHELLCHECK := $(shell which shellcheck)
+SHELL_BUILD_TARGETS := $(patsubst %.sh,%.sh.build.target,$(SHELL_FILES))
+
+SHELLCHECK_OPTS ?= -f gcc -a -x -Cnever
+
+build-shell: $(SHELL_BUILD_TARGETS)
+
+cfg-shell:
+	@$(MSG) "[CFG]" "Shell"
+	@$(CFGMSG) "SHELL_FILES:" "$(SHELL_FILES)"
+
+%.sh.build.target: %.sh
+	@$(MSG) "[SH]" "$<"
+	$(V)$(SHELLCHECK) $(SHELLCHECK_OPTS) $(SHELLCHECK_INCLUDE_PATHS:%=-P "%") $(abspath "$<")
+
+.PHONY: %.sh.build.target %.sh.test.target build-python test-python cfg-py
