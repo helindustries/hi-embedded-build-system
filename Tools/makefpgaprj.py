30818bdc01dba2fd4e8655ff4b4242eb4e67da82
Paul Schulze <pschulze@helindustries.com>
2023-06-06 01:06:21 +0200

    Fix mypy issues in makefpgaprj.py

--- Tools/makefpgaprj.py
+++ Tools/makefpgaprj.py
@@ -1,137 +1,137 @@
 #!/usr/bin/env python3
-
+from typing import List, Tuple, Optional, Match, Pattern, Self, Iterable
+from collections import OrderedDict
 import os.path
 import re
 import sys
-from collections import OrderedDict
 
-package_re = re.compile("^[\ \t]*package (?P<name>[a-zA-Z0-9_-]+) is[\ \t]*((--).*)?\r?$")
-include_re = re.compile("^[\ \t]*use (?P<library>[a-zA-Z0-9_-]+).(?P<package>[a-zA-Z0-9_-]+).[a-zA-Z0-9_-]+;[\ \t]*((--).*)?\r?$")
+package_re: Pattern = re.compile("^[\ \t]*package (?P<name>[a-zA-Z0-9_-]+) is[\ \t]*((--).*)?\r?$")
+include_re: Pattern = re.compile("^[\ \t]*use (?P<library>[a-zA-Z0-9_-]+).(?P<package>[a-zA-Z0-9_-]+).[a-zA-Z0-9_-]+;[\ \t]*((--).*)?\r?$")
 
-lse_top_template = """<?xml version="1.0" encoding="UTF-8"?>
+lse_top_template: str = """<?xml version="1.0" encoding="UTF-8"?>
 <BaliProject version="3.2" title="{top_module}" device="{package}" default_implementation="{top_module}">
     <Options/>
     <Implementation title="{top_module}" dir="{top_module}" description="{top_module}" synthesis="lse" default_strategy="Strategy1">
         <Options def_top="{top_module}" top="{top_module}"/>
 """
-lse_sim_only_template = " syn_sim=\"SimOnly\""
-lse_lib_option_template = "lib=\"helindustries\""
-lse_lib_template = """        <Source name="{source_path}" type="{source_type}" type_short="{source_short}"{sim_only}>
+lse_sim_only_template: str = " syn_sim=\"SimOnly\""
+lse_lib_option_template: str = "lib=\"helindustries\""
+lse_lib_template: str = """        <Source name="{source_path}" type="{source_type}" type_short="{source_short}"{sim_only}>
             <Options{lib_opts}/>
         </Source>
 """
-lse_bottom_template = """    </Implementation>
+lse_bottom_template: str = """    </Implementation>
     <Strategy name="Strategy" file="{top_module}.sty"/>
 </BaliProject>
 """
 
 class HardwareInclude(object):
-    def __init__(self, library, name, line):
+    def __init__(self: Self, library: str, name: str, line: int) -> None:
         super(HardwareInclude, self).__init__()
-        self.library = library.lower()
-        self.name = name.lower()
-        self.line = line
+        self.library: str = library.lower()
+        self.name: str = name.lower()
+        self.line: int = line
 
 class HardwareModule(object):
-    def __init__(self, library, name):
+    def __init__(self: Self, library: HardwareLibrary, name: str) -> None:
         super(HardwareModule, self).__init__()
-        self.library = library
-        self.name = name
-        self.key = name.lower()
-        self.packages = []
-        self.requires = []
+        self.library: HardwareLibrary = library
+        self.name: str = name
+        self.key: str = name.lower()
+        self.packages: list[str] = []
+        self.requires: list[HardwareInclude] = []
 
-    def get_path(self):
+    def get_path(self: Self) -> str:
         return os.path.join(self.library.path, self.name)
 
-    def collect(self):
-        path = self.get_path()
+    def collect(self: Self) -> None:
+        path: str = self.get_path()
         with open(path, "r") as fd:
-            content = fd.readlines()
+            content: Iterable[str] = fd.readlines()
 
         self.packages = []
         for line in content:
-            match = package_re.match(line)
+            match: Optional[Match] = package_re.match(line)
             if match is not None:
                 self.packages.append(match.groupdict()["name"].lower())
 
-        index = 0
+        index: int = 0
         self.requires = []
         for line in content:
             index += 1
             match = include_re.match(line)
             if match is not None:
-                library = match.groupdict()["library"]
-                package = match.groupdict()["package"]
+                library: str = match.groupdict()["library"]
+                package: str = match.groupdict()["package"]
                 if package.lower() not in self.packages:
                     self.requires.append(HardwareInclude(library, package, index))
 
-    def __str__(self):
+    def __str__(self: Self) -> str:
         return self.name
 
 class HardwareLibrary(object):
-    def __init__(self, name, path):
+    def __init__(self: Self, name: str, path: str) -> None:
         super(HardwareLibrary, self).__init__()
-        self.name = name
-        self.key = name.lower()
-        self.path = path
-        self.modules = OrderedDict()
-        self.modulesByPackage = OrderedDict()
+        self.name: str = name
+        self.key: str = name.lower()
+        self.path: str = path
+        self.modules: OrderedDict[str, HardwareModule] = OrderedDict()
+        self.modulesByPackage: OrderedDict[str, HardwareModule] = OrderedDict()
 
-    def add_module(self, path):
-        module = HardwareModule(self, path)
+    def add_module(self: Self, path: str) -> None:
+        module: HardwareModule = HardwareModule(self, path)
         module.collect()
         self.modules[module.key] = module
         for package in module.packages:
             self.modulesByPackage[package] = module
 
-    def collect(self):
-        path = self.path
+    def collect(self: Self) -> None:
+        path: str = self.path
         if path == "":
             path = "."
 
         self.modules = OrderedDict()
         self.modulesByPackage = OrderedDict()
         for name in os.listdir(path):
-            key = name.lower()
+            key: str = name.lower()
             if key.endswith(".vhd") or key.endswith(".vhdl") or key.endswith(".v"):
                 self.add_module(name)
 
-    def __str__(self):
+    def __str__(self: Self) -> str:
         return "%s:%s" % (self.name, self.path)
 
     @classmethod
-    def parse(cls, in_str):
+    def parse(cls: type, in_str: str) -> HardwareLibrary:
         return cls(*in_str.split(":", 1))
 
 class HardwareDesignDependencies(object):
-    def __init__(self, main_path):
+    def __init__(self: Self, main_path: str) -> None:
         super(HardwareDesignDependencies, self).__init__()
-        self.main_name = os.path.basename(main_path)
-        self.libraries = {"work": HardwareLibrary("work", os.path.dirname(main_path))}
-        self.dependenciesByFile = OrderedDict()
-        self.modules = OrderedDict()
-        self.static_paths = []
+        self.main_name: str = os.path.basename(main_path)
+        self.libraries: dict[str, HardwareLibrary] = {"work": HardwareLibrary("work", os.path.dirname(main_path))}
+        self.dependenciesByFile: OrderedDict[str, HardwareModule] = OrderedDict()
+        self.modules: OrderedDict[str, HardwareModule] = OrderedDict()
+        self.static_paths: list[str] = []
 
-    def add_library(self, library):
+    def add_library(self: Self, library: HardwareLibrary) -> None:
         self.libraries[library.name.lower()] = library
 
-    def add_static_path(self, path):
+    def add_static_path(self: Self, path: str) -> None:
         self.static_paths.append(path)
 
-    def collect(self):
+    def collect(self: Self) -> int:
         for name, library in self.libraries.items():
             library.collect()
 
-        work = self.libraries["work"]
+        work: HardwareLibrary = self.libraries["work"]
         for path in self.static_paths:
             work.add_module(path)
 
-        module = work.modules[self.main_name.lower()]
+        module: HardwareModule = work.modules[self.main_name.lower()]
         self.dependenciesByFile[module.get_path()] = module
-        stack = [module]
+        stack: list[HardwareModule] = [module]
         while len(stack) > 0:
-            dependency = stack.pop(len(stack) - 1)
+            dependency: HardwareModule = stack.pop(len(stack) - 1)
 
             for require in dependency.requires:
                 if require.library in self.libraries:
@@ -143,7 +143,7 @@ class HardwareDesignDependencies(object):
                         return 2
                     else:
                         module = self.libraries[require.library].modulesByPackage[require.name]
-                        key = module.get_path()
+                        key: str = module.get_path()
 
                         if key in self.dependenciesByFile:
                             del(self.dependenciesByFile[key])
@@ -155,13 +155,13 @@ class HardwareDesignDependencies(object):
         self.dependenciesByFile = OrderedDict(reversed(list(self.dependenciesByFile.items())))
         return 0
 
-    def copy_to(self, path):
+    def copy_to(self: Self, path: str) -> None:
         pass
 
-    def write_xilinx(self, path, relative_to):
+    def write_xilinx(self: Self, path: str, relative_to: str) -> None:
         with open(path, "w+") as fd:
             for k, v in self.dependenciesByFile.items():
-                filetype = ""
+                filetype: str = ""
                 if v.name.endswith(".vhd") or v.name.endswith(".vhdl"):
                     filetype = "vhdl"
                 elif v.name.endswith(".v"):
@@ -172,19 +172,19 @@ class HardwareDesignDependencies(object):
                     path = os.path.relpath(relative_to, path)
                 fd.write("%s %s %s\n" % (filetype, v.library.name, path))
 
-    def write_lattice(self, relative_to):
-        libname = ""
-        dependencies = list(self.dependenciesByFile.values())
+    def write_lattice(self: Self, relative_to: str) -> None:
+        libname: str = ""
+        dependencies: list[HardwareModule] = list(self.dependenciesByFile.values())
         dependencies.sort(key = lambda dependency: dependency.library.name)
 
         for dependency in dependencies:
-            filetype = ""
+            filetype: str = ""
             if dependency.name.endswith(".vhd"):
                 filetype = "-vhd"
             elif dependency.name.endswith(".v"):
                 filetype = "-ver"
 
-            path = os.path.join(dependency.library.path, dependency.name)
+            path: str = os.path.join(dependency.library.path, dependency.name)
             if relative_to == "":
                 path = os.path.abspath(path)
             else:
@@ -198,28 +198,28 @@ class HardwareDesignDependencies(object):
                 sys.stdout.write("-lib \"%s\" " % dependency.library.name)
             sys.stdout.write("%s \"%s\"\n" % (filetype, path))
 
-    def write_lse(self, relative_to, package, lpf):
-        top_module = self.main_name.rstrip(".vhd").rstrip(".v")
+    def write_lse(self: Self, relative_to: str, package: str, lpf: str) -> None:
+        top_module: str = self.main_name.rstrip(".vhd").rstrip(".v")
         sys.stdout.write(lse_top_template % {"top_module": top_module, "package": package})
 
         for k, v in self.dependenciesByFile.items():
-            path = os.path.join(v.library.path, v.name)
+            path: str = os.path.join(v.library.path, v.name)
             if relative_to == "":
                 path = os.path.abspath(path)
             else:
                 path = os.path.relpath(relative_to, path)
 
-            sim_only = ""
+            sim_only: str = ""
             if path.rstrip(".vhd").rstrip(".v").endswith("_tb"):
                 sim_only = lse_sim_only_template
 
-            source_type = ""
+            source_type: str = ""
             if path.endswith(".vhd"):
                 source_type = "VHDL"
             if path.endswith(".v"):
                 source_type = "Verilog"
 
-            lib_opts = ""
+            lib_opts: str = ""
             if v.library.name != "work":
                 lib_opts = lse_lib_option_template % {"lib_opts": v.library.name}
             sys.stdout.write(lse_lib_template % {"source_path": path, "source_type": source_type, "source_short": source_type, "lib_opts": lib_opts, "sim_only": sim_only})
@@ -227,18 +227,18 @@ class HardwareDesignDependencies(object):
         sys.stdout.write(lse_lib_template % {"source_path": lpf, "source_type": "Logic Preference", "source_short": "LPF", "lib_opts": "", "sim_only": ""})
         sys.stdout.write(lse_bottom_template % {"top_module": top_module})
 
-    def write_yosys(self, relative_to):
+    def write_yosys(self: Self, relative_to: str) -> None:
         for k, v in self.dependenciesByFile.items():
-            path = os.path.join(v.library.path, v.name)
+            path: str = os.path.join(v.library.path, v.name)
             if relative_to == "":
                 path = os.path.abspath(path)
             else:
                 path = os.path.relpath(relative_to, path)
             sys.stdout.write("%s %s\n" % (v.library.name, path))
 
-    def write_ghdl(self, relative_to):
+    def write_ghdl(self: Self, relative_to: str) -> None:
         for k, v in self.dependenciesByFile.items():
-            path = os.path.join(v.library.path, v.name)
+            path: str = os.path.join(v.library.path, v.name)
             if relative_to == "":
                 path = os.path.abspath(path)
             else:
@@ -251,7 +251,7 @@ class HardwareDesignDependencies(object):
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser()
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
     parser.add_argument("main_path", help="Path of the main file.")
     parser.add_argument("-l", "--library", metavar="LIBRARY", default="work", help="Name of the library.", dest="library")
     parser.add_argument("-o", "--outputfile", metavar="FILE", default="", help="Path of the output file.", dest="outputfile")
@@ -267,13 +267,13 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--include", metavar="NAME:PATH", action="append", help="Name and path of the included library.", dest="include")
     parser.add_argument("-s", "--static", metavar="PATH", action="append", help="Add this static path to the work module", dest="static")
     parser.add_argument("-a", "--absolute", action="store_true", help="Use absolute file paths", dest="absolute")
-    args = parser.parse_args()
+    args: argparse.Namespace = parser.parse_args()
 
     #outputfile = args.outputfile
     #if outputfile == "":
     #    outputfile = re.sub("\.(vhd)|(v)$", ".prj", args.main_path)
 
-    deps = HardwareDesignDependencies(args.main_path)
+    deps: HardwareDesignDependencies = HardwareDesignDependencies(args.main_path)
     if args.include is not None:
         for include in args.include:
             deps.add_library(HardwareLibrary.parse(include))
@@ -284,7 +284,7 @@ if __name__ == "__main__":
     if deps.collect() != 0:
         sys.exit(1)
 
-    basedir = os.path.dirname(args.outputfile)
+    basedir: str = os.path.dirname(args.outputfile)
     if args.copy and basedir != "":
         deps.copy_to(basedir)
     else:
