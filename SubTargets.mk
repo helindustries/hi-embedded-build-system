ea483ce25c06567fc00d00bc5ed439e70388fcca
Paul Schulze <pschulze@helindustries.com>
2023-06-06 00:45:04 +0200

    Replace " by ' in SubTargets.mk, so we don't run into issues with quoted values

--- SubTargets.mk
+++ SubTargets.mk
@@ -1,8 +1,8 @@
 $(TOPTARGETS): $(SUBDIRS)
 $(SUBDIRS):
-	$(V)$(VMSG) "Building Subtarget $@"
-	$(V)$(VCFGMSG) "NAME:" "$*"
-	$(V)$(VCFGMSG) "BUILD_DIR:" "$(BUILD_DIR)"
-	$(V)$(MAKE) --directory="$(abspath $@)" --file "$(abspath $@)/Makefile" "BUILD_DIR=$(BUILD_DIR)/$@" "VERBOSE=$(VERBOSE)" "NO_GATEWARE_DEPS=$(NO_GATEWARE_DEPS)" "NO_TOOLS_DEPS=$(NO_TOOLS_DEPS)" "NO_TESTS_DEPS=$(NO_TESTS_DEPS)" $(MAKECMDGOALS)
+	@$(VMSG) "Building Subtarget $@"
+	@$(VCFGMSG) 'NAME:' '$*'
+	@$(VCFGMSG) 'BUILD_DIR:' '$(BUILD_DIR)'
+	$(V)$(MAKE) --directory='$(abspath $@)' --file='$(abspath $@)/Makefile' 'BUILD_DIR=$(BUILD_DIR)/$@' 'VERBOSE=$(VERBOSE)' 'NO_GATEWARE_DEPS=$(NO_GATEWARE_DEPS)' 'NO_TOOLS_DEPS=$(NO_TOOLS_DEPS)' 'NO_TESTS_DEPS=$(NO_TESTS_DEPS)' $(MAKECMDGOALS)
 
 .PHONY: $(TOPTARGETS) $(SUBDIRS)
