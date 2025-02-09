989e310a4e577670a173b5caad4b1ecd17eae08b
Paul Schulze <pschulze@helindustries.com>
2025-02-09 11:25:22 +0100

    Fix clean rule to actually remove module libraries properly

--- MCUTargets.mk
+++ MCUTargets.mk
@@ -34,7 +34,7 @@ upload-mcu: binary-mcu upload_$(MCU_BOARD)$(MCU_JTAG_UPLOAD_TARGET) | silent
 
 clean-mcu: clean-base clean-modules clean_${MCU_TOOLCHAIN}
 	@$(MSG) "[CLEAN]" "$(MCU_TARGET)"
-	$(V)rm -f $(BUILD_DIR)/$(MCU_TARGET)-$(MCU).elf	$(BUILD_DIR)/$(MCU_TARGET)-$(MCU)
+	$(V)rm -f $(BUILD_DIR)/$(MCU_TARGET)-$(MCU).elf	$(BUILD_DIR)/lib$(MCU_TARGET)-$(MCU).a $(BUILD_DIR)/$(MCU_TARGET)-$(MCU)
 ifneq ($(strip $(ELF_MAP)),)
 	$(V)rm -f $(BUILD_DIR)/$(ELF_MAP)
 endif
