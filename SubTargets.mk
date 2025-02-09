7812542db77d7f0b05197c1f5b06a99b85f82815
Paul Schulze <pschulze@helindustries.com>
2025-02-09 11:16:31 +0100

    Fix argument order when calling subtarget Makefile

--- SubTargets.mk
+++ SubTargets.mk
@@ -3,6 +3,6 @@ $(SUBDIRS):
 	@$(VMSG) "Building Subtarget $@"
 	@$(VCFGMSG) 'NAME:' '$*'
 	@$(VCFGMSG) 'BUILD_DIR:' '$(BUILD_DIR)'
-	$(V)$(MAKE) --directory='$(abspath $@)' --file='$(abspath $@)/Makefile' 'BUILD_DIR=$(BUILD_DIR)/$@' 'VERBOSE=$(VERBOSE)' 'NO_GATEWARE_DEPS=$(NO_GATEWARE_DEPS)' 'NO_TOOLS_DEPS=$(NO_TOOLS_DEPS)' 'NO_TESTS_DEPS=$(NO_TESTS_DEPS)' $(MAKECMDGOALS)
+	$(V)$(MAKE) --directory='$(abspath $@)' --file='$(abspath $@)/Makefile' 'BUILD_DIR=$(BUILD_DIR)/$@' 'VERBOSE=$(VERBOSE)' 'NO_GATEWARE_DEPS=$(NO_GATEWARE_DEPS)' 'NO_TOOLS_DEPS=$(NO_TOOLS_DEPS)' 'NO_TESTS_DEPS=$(NO_TESTS_DEPS)' $(MAKEFLAGS) $(MAKECMDGOALS)
 
 .PHONY: $(TOPTARGETS) $(SUBDIRS)
