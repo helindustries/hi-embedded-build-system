76d09669102564415686464b66047a8b7fb0f80d
Paul Schulze <pschulze@helindustries.com>
2025-02-09 11:28:00 +0100

    Fix core and Arduino defines not conforming completely to the IDE standards

--- MCUFlags.mk
+++ MCUFlags.mk
@@ -15,19 +15,19 @@ INCLUDE_PATHS += "$(abspath $(MAKE_BASE_PATH))"
 
 # Project-specific board definitions
 ifneq ($(strip $(BOARDS_DIR)),)
-	HEADERS -= $(filter $(wildcard $(BOARDS_DIR)/*.h),$(HEADERS))
-	CPP_FILES -= $(filter $(wildcard $(BOARDS_DIR)/*.cpp),$(CPP_FILES))
-	C_FILES -= $(filter $(wildcard $(BOARDS_DIR)/*.c),$(C_FILES))
-	ASM_FILES -= $(filter $(wildcard $(BOARDS_DIR)/*.s),$(ASM_FILES))
-	ASM_FILES -= $(filter $(wildcard $(BOARDS_DIR)/*.S),$(ASM_FILES))
+    HEADERS -= $(filter $(wildcard $(BOARDS_DIR)/*.h),$(HEADERS))
+    CPP_FILES -= $(filter $(wildcard $(BOARDS_DIR)/*.cpp),$(CPP_FILES))
+    C_FILES -= $(filter $(wildcard $(BOARDS_DIR)/*.c),$(C_FILES))
+    ASM_FILES -= $(filter $(wildcard $(BOARDS_DIR)/*.s),$(ASM_FILES))
+    ASM_FILES -= $(filter $(wildcard $(BOARDS_DIR)/*.S),$(ASM_FILES))
 
-	HEADERS += $(wildcard $(BOARDS_DIR)/$(MCU_BOARD)/*.h)
-	CPP_FILES += $(wildcard $(BOARDS_DIR)/$(MCU_BOARD)/*.cpp)
-	C_FILES += $(wildcard $(BOARDS_DIR)/$(MCU_BOARD)/*.c)
-	ASM_FILES += $(wildcard $(BOARDS_DIR)/$(MCU_BOARD)/*.s)
-	ASM_FILES += $(wildcard $(BOARDS_DIR)/$(MCU_BOARD)/*.S)
+    HEADERS += $(wildcard $(BOARDS_DIR)/$(MCU_BOARD)/*.h)
+    CPP_FILES += $(wildcard $(BOARDS_DIR)/$(MCU_BOARD)/*.cpp)
+    C_FILES += $(wildcard $(BOARDS_DIR)/$(MCU_BOARD)/*.c)
+    ASM_FILES += $(wildcard $(BOARDS_DIR)/$(MCU_BOARD)/*.s)
+    ASM_FILES += $(wildcard $(BOARDS_DIR)/$(MCU_BOARD)/*.S)
 
-	INCLUDE_PATHS += "$(BOARDS_DIR)/$(MCU_BOARD)"
+    INCLUDE_PATHS += "$(BOARDS_DIR)/$(MCU_BOARD)"
 endif
 
 include $(MAKE_INC_PATH)/Targets/MCU/$(MCU_BOARD).mk
@@ -39,9 +39,12 @@ MCU_ARCH := $(shell echo $(MCU_TOOLCHAIN) | tr '[:lower:]' '[:upper:]')
 F_CPU := $(CPU_SPEED)000000
 F_BUS := $(BUS_SPEED)000000
 
-CPPFLAGS += -D__$(MCU_UPPER)__ -DF_CPU=$(F_CPU) $(MCU_BOARD_OPTS) -DCORE_PLATFORM_$(CORE_PLATFORM)
+CORE_PLATFORM_UPPER := $(shell echo $(CORE_PLATFORM) | tr '[:lower:]' '[:upper:]')
 ASMFLAGS += -x assembler-with-cpp
-ifneq (BUS_SPEED,)
+CPPFLAGS += -DF_CPU=$(F_CPU) $(MCU_BOARD_OPTS) -D__$(MCU_UPPER)__
+CPPFLAGS += -DCORE_PLATFORM_$(CORE_PLATFORM_UPPER) -DCORE_PLATFORM=\"$(CORE_PLATFORM)\"
+CPPFLAGS += -DCPU_NAME=\"$(strip $(MCU_UPPER))\" -DBOARD_NAME=\"$(strip $(MCU_BOARD))\"
+ifneq ($(strip $(BUS_SPEED)),)
 	CPPFLAGS += -DF_BUS=$(F_BUS)
 endif
 ifeq ($(strip $(USB)),yes)
@@ -56,17 +59,19 @@ endif
 
 # Base Arduino compatibility
 ifeq ($(strip $(USE_ARDUINO_CORE)),yes)
-	CPPFLAGS += -DARDUINO=10812 -DTEENSYDUINO=151 -DARDUINO_$(strip $(MCU_BOARD_UPPER)) -DARDUINO_BOARD=\"$(strip $(MCU_BOARD_UPPER))\" -DARDUINO_ARCH_$(CORE_PLATFORM)
+    CPPFLAGS += -DARDUINO=10812 -DARDUINO_ARCH_$(CORE_PLATFORM_UPPER)
+    CPPFLAGS += -DARDUINO_$(strip $(MCU_BOARD_UPPER)) -DARDUINO_BOARD=\"$(strip $(MCU_BOARD_UPPER))\"
 
-	ifneq ($(strip $(ARDUINO_VARIANT_NAME)),)
-		ifneq ($(strip $(CORE_VARIANTS_PATH)),)
-			INCLUDE_PATHS += "$(CORE_VARIANTS_PATH)/$(ARDUINO_VARIANT_NAME)"
-			LIBRARY_PATHS += "$(CORE_VARIANTS_PATH)/$(ARDUINO_VARIANT_NAME)"
+    ifneq ($(strip $(ARDUINO_VARIANT_NAME)),)
+    	CPPFLAGS += -DARDUINO_VARIANT=\"$(ARDUINO_VARIANT_NAME)\"
+        ifneq ($(strip $(CORE_VARIANTS_PATH)),)
+            INCLUDE_PATHS += "$(CORE_VARIANTS_PATH)/$(ARDUINO_VARIANT_NAME)"
+            LIBRARY_PATHS += "$(CORE_VARIANTS_PATH)/$(ARDUINO_VARIANT_NAME)"
 
-			HEADERS += $(wildcard $(CORE_VARIANTS_PATH)/$(ARDUINO_VARIANT_NAME)/*.h)
-			CPP_FILES += $(wildcard $(CORE_VARIANTS_PATH)/$(ARDUINO_VARIANT_NAME)/*.cpp)
-			C_FILES += $(wildcard $(CORE_VARIANTS_PATH)/$(ARDUINO_VARIANT_NAME)/*.c)
-			ASM_FILES += $(wildcard $(CORE_VARIANTS_PATH)/$(ARDUINO_VARIANT_NAME)/*.s)
-		endif
-	endif
+            HEADERS += $(wildcard $(CORE_VARIANTS_PATH)/$(ARDUINO_VARIANT_NAME)/*.h)
+            CPP_FILES += $(wildcard $(CORE_VARIANTS_PATH)/$(ARDUINO_VARIANT_NAME)/*.cpp)
+            C_FILES += $(wildcard $(CORE_VARIANTS_PATH)/$(ARDUINO_VARIANT_NAME)/*.c)
+            ASM_FILES += $(wildcard $(CORE_VARIANTS_PATH)/$(ARDUINO_VARIANT_NAME)/*.s)
+        endif
+    endif
 endif
