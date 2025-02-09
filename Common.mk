5b1975e6b65bc4ece9ba109e2825fa3c7f4aeaf4
Paul Schulze <pschulze@helindustries.com>
2025-02-09 11:13:32 +0100

    Add preliminary platform detection

--- Common.mk
+++ Common.mk
@@ -16,6 +16,16 @@ else
 	VCFGMSG = printf "  %-9s %s\n"
 endif
 
+ifeq ($(strip $(shell uname -s)),Darwin)
+	PLATFORM = MacOS
+else
+ifeq ($(strip $(shell uname -s)),Linux)
+	PLATFORM = Linux
+else
+	PLATFORM = Windows
+endif
+endif
+
 silent:
 	@:
 
