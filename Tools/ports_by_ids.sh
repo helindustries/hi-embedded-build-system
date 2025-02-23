7d99657ca746d60b4dd77393b7f1919f5414b8eb
Paul Schulze <pschulze@helindustries.com>
2025-02-23 03:33:22 +0100

    Fix ports_by_ids not using the correct ID formatting and failing

--- Tools/ports_by_ids.sh
+++ Tools/ports_by_ids.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-LOWER_PID=$(echo "$2" | tr '[:upper:]' '[:lower:]')
-LOWER_VID=$(echo "$3" | tr '[:upper:]' '[:lower:]')
-ls --color=never /dev/cu.usb* | sed -E 's&/dev/cu\.usb[a-z]+([0-9]+)[0-9]$&\1&' | grep -v "/dev/cu.usb" | xargs -I {} bash -c 'system_profiler SPUSBDataType | grep -B 6 "$1" | grep -A 1 "Product ID: $LOWER_PID" | grep "Vendor ID: $LOWER_VID" > /dev/null && ls --color=never /dev/cu.usb*$1* | head -n 1' _ {} $1 $2
+LOWER_PID=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+LOWER_VID=$(echo "$2" | tr '[:upper:]' '[:lower:]')
+shift 2
+for port in "$@"
+do
+    port_base="$(echo "$port" | sed -E 's&/dev/cu\.usb[a-z]+([0-9]+)[0-9]$&\1&')"
+    if system_profiler SPUSBDataType 2>/dev/null | grep -B 6 "$port_base" | grep -A 1 "Product ID: $LOWER_PID" | grep "Vendor ID: $LOWER_VID" > /dev/null
+    then
+        echo "$port"
+    fi
+done
