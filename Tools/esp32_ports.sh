759b3bd2dfb296fe3f933bd36f03190846570442
Paul Schulze <pschulze@helindustries.com>
2025-02-23 03:35:25 +0100

    Make the ESP32 port detection use ports_by_ids as a pre-filter because it is significantly faster

--- Tools/esp32_ports.sh
+++ Tools/esp32_ports.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+DIR="$(dirname "$0")"
 ESPTOOL=$1
-shift 1
+PID=$2
+VID=$3
+shift 3
 export LANG=C
 export LC_ALL=C
 
-for port in "$@"
+for port in $(bash $DIR/ports_by_ids.sh $PID $VID "$@")
 do
     if "$ESPTOOL" --port "$port" --baud 115200 --connect-attempts 2 chip_id | grep "Connecting\." >/dev/null 2>&1
     then
