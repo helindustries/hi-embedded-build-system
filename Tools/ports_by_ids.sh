3fa8b092f4d67235c01b145ede917f874f41dec8
Paul Schulze <avlex82@gmail.com>
2025-02-27 02:34:27 +0100

    Add support for finding a port path by the device serial on macos

--- Tools/ports_by_ids.sh
+++ Tools/ports_by_ids.sh
@@ -6,7 +6,14 @@ shift 2
 for port in "$@"
 do
     port_base="$(echo "$port" | sed -E 's&/dev/cu\.usb[a-z]+([0-9]+)[0-9]$&\1&')"
-    if system_profiler SPUSBDataType 2>/dev/null | grep -B 6 "$port_base" | grep -A 1 "Product ID: $LOWER_PID" | grep "Vendor ID: $LOWER_VID" > /dev/null
+    if system_profiler SPUSBDataType 2>/dev/null | egrep -B 6 "Location ID:.*$port_base" | grep -A 1 "Product ID: $LOWER_PID" | grep "Vendor ID: $LOWER_VID" > /dev/null
+    then
+        echo "$port"
+        continue
+    fi
+
+    port_serial="$(echo "$port" | sed -E 's&/dev/cu\.usb[a-z]+-([0-9A-F]+)$&\1&')"
+    if system_profiler SPUSBDataType 2>/dev/null | grep -B 3 "Serial Number: $port_serial" | grep -A 1 "Product ID: $LOWER_PID" | grep "Vendor ID: $LOWER_VID" > /dev/null
     then
         echo "$port"
     fi
