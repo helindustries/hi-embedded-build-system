47dc8fe8a828f06a8e597517c10e98fba73c0568
Paul Schulze <pschulze@helindustries.com>
2025-04-06 14:11:22 +0200

    Add build platform executable variables

--- Common.mk
+++ Common.mk
@@ -4,27 +4,37 @@ export LC_ALL=en_US.UTF-8
 VERBOSE ?= 0
 CFGMSG = printf "    %-30s %s\n"
 ifneq "$(VERBOSE)" "1"
-	V = @
-	MSG = printf "  %-9s    %-20s  %s\n"
-	FMSG = /usr/bin/true
-	VMSG = /usr/bin/true
-	VCFGMSG = /usr/bin/true
+    V = @
+    MSG = printf "  %-9s    %-20s  %s\n"
+    FMSG = /usr/bin/true
+    VMSG = /usr/bin/true
+    VCFGMSG = /usr/bin/true
 else
-	MSG = /usr/bin/true
-	FMSG = printf "%s\n"
-	VMSG = printf "%s\n"
-	VCFGMSG = printf "  %-9s %s\n"
+    MSG = /usr/bin/true
+    FMSG = printf "%s\n"
+    VMSG = printf "%s\n"
+    VCFGMSG = printf "  %-9s %s\n"
 endif
 
 ifeq ($(strip $(shell uname -s)),Darwin)
-	PLATFORM = MacOS
+    PLATFORM = MacOS
+    # XXX: Add app package support once we move to a functional sim
+    #PLATFORM_EXEC_OPEN = open
+    #PLATFORM_EXEC_EXT = .app
+    PLATFORM_EXEC_OPEN =
+    PLATFORM_EXEC_EXT =
 else
 ifeq ($(strip $(shell uname -s)),Linux)
-	PLATFORM = Linux
+    PLATFORM = Linux
+    PLATFORM_EXEC_OPEN =
+    PLATFORM_EXEC_EXT =
 else
-	PLATFORM = Windows
+    PLATFORM = Windows
+    PLATFORM_EXEC_OPEN =
+    PLATFORM_EXEC_EXT = .exe
 endif
 endif
+PLATFORM_ID = $(echo "$(PLATFORM)" | tr '[:upper:]' '[:lower:]')
 CPU_BASE_TARGET ?= $(CPU_TARGET)
 
 silent:
