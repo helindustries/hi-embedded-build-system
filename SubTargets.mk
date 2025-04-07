4d2fc55d5ecf76c2284337942a6533ed2cac0452
Paul Schulze <pschulze@helindustries.com>
2025-04-07 02:53:02 +0200

    Fix verbose output of subtarget builds

--- SubTargets.mk
+++ SubTargets.mk
@@ -1,7 +1,7 @@
 $(TOPTARGETS): $(SUBDIRS)
 $(SUBDIRS):
-	@$(VMSG) "Building Subtarget $@"
-	@$(VCFGMSG) 'NAME:' '$*'
+	@$(VMSG) "Building Subtarget $(patsubst %/,%,$@)"
+	@$(VCFGMSG) 'NAME:' '$(patsubst %/,%,$@)'
 	@$(VCFGMSG) 'BUILD_DIR:' '$(BUILD_DIR)'
 	$(V)$(MAKE) --directory='$(abspath $@)' --file='$(abspath $@)/Makefile' $(MAKEFLAGS) 'STARTUP_MAKEFILE=$(STARTUP_MAKEFILE)' 'BUILD_DIR=$(BUILD_DIR)/$@' \
 					'VERBOSE=$(VERBOSE)' 'NO_GATEWARE_DEPS=$(NO_GATEWARE_DEPS)' 'NO_TOOLS_DEPS=$(NO_TOOLS_DEPS)' 'NO_TESTS_DEPS=$(NO_TESTS_DEPS)' \
