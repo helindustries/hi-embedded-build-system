5d2816ee2ca811fb86c922d5aaedcae6703b4a84
Paul Schulze <pschulze@helindustries.com>
2025-04-07 03:02:23 +0200

    Fix formatting on SubTargets.mk call of sub-makefile

--- SubTargets.mk
+++ SubTargets.mk
@@ -3,8 +3,6 @@ $(SUBDIRS):
 	@$(VMSG) "Building Subtarget $(patsubst %/,%,$@)"
 	@$(VCFGMSG) 'NAME:' '$(patsubst %/,%,$@)'
 	@$(VCFGMSG) 'BUILD_DIR:' '$(BUILD_DIR)'
-	$(V)$(MAKE) --directory='$(abspath $@)' --file='$(abspath $@)/Makefile' $(MAKEFLAGS) 'STARTUP_MAKEFILE=$(STARTUP_MAKEFILE)' 'BUILD_DIR=$(BUILD_DIR)/$@' \
-					'VERBOSE=$(VERBOSE)' 'NO_GATEWARE_DEPS=$(NO_GATEWARE_DEPS)' 'NO_TOOLS_DEPS=$(NO_TOOLS_DEPS)' 'NO_TESTS_DEPS=$(NO_TESTS_DEPS)' \
-					'CPU_BASE_TARGET=$(CPU_BASE_TARGET)' $(MAKECMDGOALS)
+	$(V)$(MAKE) --directory='$(abspath $@)' --file='$(abspath $@)/Makefile' $(MAKEFLAGS) 'BUILD_DIR=$(BUILD_DIR)/$(patsubst %/,%,$@)' $(MAKECMDGOALS)
 
 .PHONY: $(TOPTARGETS) $(SUBDIRS)
