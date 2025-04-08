b51f257233f32999ab896c7d7014b3ba84c094fc
Paul Schulze <pschulze@helindustries.com>
2025-04-08 21:07:33 +0200

    Make Subtargets log properly

--- SubTargets.mk
+++ SubTargets.mk
@@ -1,8 +1,14 @@
 $(TOPTARGETS): $(SUBDIRS)
 $(SUBDIRS):
+ifeq ($(strip $(VERBOSE)),1)
 	@$(VMSG) "Building Subtarget $(patsubst %/,%,$@)"
 	@$(VCFGMSG) 'NAME:' '$(patsubst %/,%,$@)'
 	@$(VCFGMSG) 'BUILD_DIR:' '$(BUILD_DIR)'
+	@$(VCFGMSG) 'MAKEFLAGS:' '$(MAKEFLAGS)'
+	@$(VCFGMSG) 'MAKECMDGOALS:' '$(MAKECMDGOALS)'
+else
+	@$(MSG) "[MAKE]" "$(patsubst %/,%,$@)"
+endif
 	$(V)$(MAKE) --directory='$(abspath $@)' --file='$(abspath $@)/Makefile' $(MAKEFLAGS) 'BUILD_DIR=$(BUILD_DIR)/$(patsubst %/,%,$@)' $(MAKECMDGOALS)
 
 .PHONY: $(TOPTARGETS) $(SUBDIRS)
