d4c3edfe14fe6990dfdfab819bdb5d2c1e668c53
Paul Schulze <pschulze@helindustries.com>
2025-04-14 01:40:50 +0200

    Add initial (untested) support for RP2350 and RP in general

---
+++ Arduino/Devices/feather-rp2350-hstx.mk
@@ -0,0 +1,2 @@
+ARDUINO_VARIANT_NAME := adafruit_feather_rp2350_hstx
+CPU_WAIT_FOR_BOARD_PORT = yes
---
+++ Devices/CPU/Base/RPI.mk
@@ -0,0 +1,116 @@
+CORE_PLATFORM := RPI
+RPI_TOOLCHAIN_DIR ?= $(ARDUINO_USERPATH)/packages/rp2040
+RPI_BASE_PATH ?= $(strip $(shell $(LS) -d "$(RPI_TOOLCHAIN_DIR)/hardware/rp2040"/* 2>/dev/null | sort | tail -n 1))
+CORE_PATH ?= $(RPI_BASE_PATH)/cores/rp2040
+CORE_LIB_PATH ?= $(RPI_BASE_PATH)/libraries
+CORE_VARIANTS_PATH ?= $(RPI_BASE_PATH)/variants
+ARM_COMPILERPATH ?= $(strip $(shell $(LS) -d "$(RPI_TOOLCHAIN_DIR)/tools/pqt-gcc"/*/bin 2>/dev/null | sort | tail -n 1))
+ELF_MAP := $(CPU_TARGET).$(CPU_DEVICE).map
+USE_DEFAULT_USB_SERIAL_DETECT := yes
+RPI_UF2_UPLOAD_FAMILY := RP2040
+CORE_SKIP_NEW_O := yes
+ARM_USE_CMSIS := no
+
+RPI_TOOLS_DIR := $(RPI_BASE_PATH)/tools
+RPI_UF2CONV := $(RPI_TOOLS_DIR)/uf2conv.py
+RPI_SIGNING := $(RPI_TOOLS_DIR)/signing.py
+RPI_SIMPLESUB := $(RPI_TOOLS_DIR)/simplesub.py
+RPI_PICOTOOL := $(strip $(shell $(LS) -d "$(RPI_TOOLCHAIN_DIR)/tools/pqt-picotool"/*/picotool 2>/dev/null | sort | tail -n 1))
+RPI_SDK_PATH := $(RPI_BASE_PATH)/pico-sdk
+RPI_SIGNING_PRIVATE_KEY := "$(abspath $(dir $(STARTUP_MAKEFILE))/private.key)"
+ARM_LD_SOURCE ?= $(RPI_BASE_PATH)/lib/$(CPU)/memmap_default.ld
+ARM_LD ?= $(BUILD_DIR)/$(notdir $(ARM_LD_SOURCE))
+BINARY_DEPS += $(ARM_LD)
+
+include $(MAKE_INC_PATH)/Architectures/ARM/Toolchain.mk
+
+CFLAGS += -std=gnu11
+CPPFLAGS += -Werror=return-type -Wno-psabi
+CPPFLAGS += -DARDUINO_ARCH_RP2040 -DLWIP_IPV6=0 -DLWIP_IPV4=1 -DLWIP_IGMP=1 -DLWIP_CHECKSUM_CTRL_PER_NETIF=1
+CPPFLAGS += -DPICO_FLASH_SIZE_BYTES=8388608 -DFILE_COPY_CONSTRUCTOR_SELECT=FILE_COPY_CONSTRUCTOR_PUBLIC -DUSE_UTF8_LONG_NAMES=1
+CPPFLAGS += -DDISABLE_FS_H_WARNING=1 -DCYW43_LWIP=1 -DCYW43_PIO_CLOCK_DIV_DYNAMIC=1 -DWIFICC=CYW43_COUNTRY_WORLDWIDE
+CPPFLAGS += -DCFG_TUSB_MCU=OPT_MCU_RP2040 -DCFG_TUSB_OS=OPT_OS_PICO -DLIB_BOOT_STAGE2_HEADERS=1 -DLIB_PICO_BIT_OPS=1
+CPPFLAGS += -DLIB_PICO_BIT_OPS_PICO=1 -DLIB_PICO_CLIB_INTERFACE=1 -DLIB_PICO_CRT0=1 -DLIB_PICO_CXX_OPTIONS=1 -DLIB_PICO_DIVIDER=1
+CPPFLAGS += -DLIB_PICO_DIVIDER_COMPILER=1 -DLIB_PICO_DOUBLE=1 -DLIB_PICO_DOUBLE_PICO=1 -DLIB_PICO_FIX_RP2040_USB_DEVICE_ENUMERATION=1
+CPPFLAGS += -DLIB_PICO_FLOAT=1 -DLIB_PICO_FLOAT_PICO=1 -DLIB_PICO_FLOAT_PICO_VFP=1 -DLIB_PICO_INT64_OPS=1 -DLIB_PICO_INT64_OPS_COMPILER=1
+CPPFLAGS += -DLIB_PICO_MEM_OPS=1 -DLIB_PICO_MEM_OPS_COMPILER=1 -DLIB_PICO_NEWLIB_INTERFACE=1 -DLIB_PICO_PLATFORM=1
+CPPFLAGS += -DLIB_PICO_PLATFORM_COMPILER=1 -DLIB_PICO_PLATFORM_PANIC=1 -DLIB_PICO_PLATFORM_SECTIONS=1 -DLIB_PICO_RUNTIME=1
+CPPFLAGS += -DLIB_PICO_RUNTIME_INIT=1 -DLIB_PICO_STANDARD_BINARY_INFO=1 -DLIB_PICO_STANDARD_LINK=1 -DLIB_PICO_SYNC=1
+CPPFLAGS += -DLIB_PICO_TIME=1 -DLIB_PICO_TIME_ADAPTER=1 -DLIB_PICO_UNIQUE_ID=1 -DLIB_PICO_UTIL=1 -DPICO_32BIT=1 -DPICO_BUILD=1
+CPPFLAGS += -DPICO_COPY_TO_RAM=0 -DPICO_CXX_ENABLE_EXCEPTIONS=0 -DPICO_NO_FLASH=0 -DPICO_NO_HARDWARE=0 -DPICO_ON_DEVICE=1
+
+RPI_LD_UNDEFINED += runtime_init_install_ram_vector_table __pre_init_runtime_init_clocks __pre_init_runtime_init_bootrom_reset
+RPI_LD_UNDEFINED += __pre_init_runtime_init_early_resets __pre_init_runtime_init_usb_power_down __pre_init_runtime_init_clocks
+RPI_LD_UNDEFINED += __pre_init_runtime_init_post_clock_resets __pre_init_runtime_init_spin_locks_reset __pre_init_runtime_init_boot_locks_reset
+RPI_LD_UNDEFINED += __pre_init_runtime_init_bootrom_locking_enable __pre_init_runtime_init_mutex __pre_init_runtime_init_default_alarm_pool
+RPI_LD_UNDEFINED += __pre_init_first_per_core_initializer __pre_init_runtime_init_per_core_bootrom_reset
+RPI_LD_UNDEFINED += __pre_init_runtime_init_per_core_h3_irq_registers __pre_init_runtime_init_per_core_irq_priorities
+
+LDFLAGS += -Wl,--warn-section-align -Wl,--wrap=malloc,--wrap=free,--wrap=realloc,--wrap=calloc -u _printf_float -u_scanf_float
+LDFLAGS += $(RPI_LD_UNDEFINED:%=-Wl,-u,%)
+#LDFLAGS += -Wl,--no-warn-rwx-segments
+ARFLAGS := -rcs
+
+RPI_SDK_INCLUDES += src/$(CPU)/hardware_regs/include src/$(CPU)/hardware_structs/include src/$(CPU)/pico_platform/include
+RPI_SDK_INCLUDES += src/boards/include src/common/hardware_claim/include src/common/pico_base/include
+RPI_SDK_INCLUDES += src/common/pico_base_headers/include src/common/pico_binary_info/include src/common/pico_bit_ops/include
+RPI_SDK_INCLUDES += src/common/pico_divider/include src/common/pico_stdlib/include src/common/pico_sync/include
+RPI_SDK_INCLUDES += src/common/pico_time/include src/common/pico_usb_reset_interface/include src/common/pico_util/include
+RPI_SDK_INCLUDES += src/common/pico_stdlib_headers/include src/common/pico_usb_reset_interface_headers/include
+RPI_SDK_INCLUDES += src/rp2_common/cmsis/stub/CMSIS/Device/RP2350/Include src/rp2_common/hardware_sha256/include
+RPI_SDK_INCLUDES += src/rp2_common/pico_sha256/include src/rp2_common/pico_btstack/include src/rp2_common/pico_cyw43_arch/include
+RPI_SDK_INCLUDES += src/rp2_common/pico_cyw43_driver/include src/rp2_common/boot_bootrom_headers/include src/rp2_common/cmsis/include
+RPI_SDK_INCLUDES += src/rp2_common/cmsis/stub/CMSIS/Core/Include src/rp2_common/hardware_adc/include src/rp2_common/hardware_base/include
+RPI_SDK_INCLUDES += src/rp2_common/hardware_boot_lock/include src/rp2_common/hardware_clocks/include src/rp2_common/hardware_divider/include
+RPI_SDK_INCLUDES += src/rp2_common/hardware_dma/include src/rp2_common/hardware_exception/include src/rp2_common/hardware_flash/include
+RPI_SDK_INCLUDES += src/rp2_common/hardware_gpio/include src/rp2_common/hardware_i2c/include src/rp2_common/hardware_interp/include
+RPI_SDK_INCLUDES += src/rp2_common/hardware_irq/include src/rp2_common/hardware_rtc/include src/rp2_common/hardware_pio/include
+RPI_SDK_INCLUDES += src/rp2_common/hardware_pll/include src/rp2_common/hardware_pwm/include src/rp2_common/hardware_resets/include
+RPI_SDK_INCLUDES += src/rp2_common/hardware_spi/include src/rp2_common/hardware_sync/include src/rp2_common/hardware_sync_spin_lock/include
+RPI_SDK_INCLUDES += src/rp2_common/hardware_timer/include src/rp2_common/hardware_uart/include src/rp2_common/hardware_vreg/include
+RPI_SDK_INCLUDES += src/rp2_common/hardware_watchdog/include src/rp2_common/hardware_xosc/include src/rp2_common/pico_aon_timer/include
+RPI_SDK_INCLUDES += src/rp2_common/pico_async_context/include src/rp2_common/pico_bootrom/include src/rp2_common/pico_double/include
+RPI_SDK_INCLUDES += src/rp2_common/pico_fix/rp2040_usb_device_enumeration/include src/rp2_common/pico_flash/include
+RPI_SDK_INCLUDES += src/rp2_common/pico_float/include src/rp2_common/pico_int64_ops/include src/rp2_common/pico_lwip/include
+RPI_SDK_INCLUDES += src/rp2_common/pico_multicore/include src/rp2_common/pico_platform/include src/rp2_common/pico_platform_compiler/include
+RPI_SDK_INCLUDES += src/rp2_common/pico_platform_sections/include src/rp2_common/pico_platform_panic/include src/rp2_common/pico_printf/include
+RPI_SDK_INCLUDES += src/rp2_common/pico_runtime/include src/rp2_common/pico_runtime_init/include src/rp2_common/pico_rand/include
+RPI_SDK_INCLUDES += src/rp2_common/pico_stdio/include src/rp2_common/pico_stdio_uart/include src/rp2_common/pico_unique_id/include
+RPI_SDK_INCLUDES += lib/lwip/src/include lib/cyw43-driver/src lib/btstack/src lib/btstack/3rd-party/bluedroid/decoder/include
+RPI_SDK_INCLUDES += lib/btstack/3rd-party/bluedroid/encoder/include lib/btstack/3rd-party/yxml lib/btstack/platform/embedded lib/tinyusb/src
+INCLUDE_PATHS += $(RPI_SDK_INCLUDES:%=$(RPI_SDK_PATH)/%)
+INCLUDE_PATHS += $(RPI_INCLUDE_BASE_PATH)/include/$(CPU)
+INCLUDE_PATHS += $(RPI_INCLUDE_BASE_PATH)/include/$(CPU)/pico_base
+INCLUDE_PATHS += $(CORE_PATH)/api/deprecated-avr-comp
+INCLUDE_PATHS += $(CORE_PATH)/include
+
+include $(MAKE_INC_PATH)/Architectures/ARM/Targets.mk
+
+%.uf2: %.elf $(SOURCES) $(DEPENDENCY_LIB_PATHS) $(MODULES_LIBS) resetter
+	@$(MSG) "[UF2]" "$(CPU_TARGET)" "$(subst $(abspath .)/,,$@)"
+	$(V)$(RPI_PICOTOOL) uf2 convert "$<" "$@" --family $(RPI_PICOTOOL_FAMILY) --abs-block > /dev/null
+
+%.bin.signed: %.bin $(SOURCES) $(DEPENDENCY_LIB_PATHS) $(MODULES_LIBS)
+	@$(MSG) "[UF2.SIGN]" "$(CPU_TARGET)" "$(subst $(abspath .)/,,$@)"
+	$(V)$(PYTHON) -I "$(RPI_SIGNING)" "$(RPI_SIGNING_PRIVATE_KEY)" --bin "$<" --out "$@" > /dev/null
+
+$(ARM_LD): $(ARM_LD_SOURCE)
+	@$(MSG) "[UF2.SIGN]" "$(CPU_TARGET)" "$(subst $(abspath .)/,,$@)"
+	$(V)$(PYTHON) -I $(RPI_SIMPLESUB) --input "$<" --out "$@" --sub __FLASH_LENGTH__ $(RPI_FLASH_LENGTH) --sub __EEPROM_START__ $(RPI_EEPROM_START) \
+			 --sub __FS_START__ $(RPI_FS_START) --sub __FS_END__ $(RPI_FS_END) --sub __RAM_LENGTH__ $(RPI_RAM_SIZE) --sub __PSRAM_LENGTH__ $(RPI_PSRAM_SIZE)
+
+%.uf2.upload_rpi.timestamp: %.uf2 serial | silent
+ifneq ($(strip $(NO_FIRMWARE_UPLOAD)),yes)
+	@$(FMSG) "INFO:Uploading $<"
+	@$(MSG) "[UPLOAD]" "$(CPU_TARGET)" "$(subst $(abspath .)/,,$<)"
+
+	$(V)$(PYTHON) -I $(UF2CONV) --serial $(CPU_DEVICE_PORT) --family $(RPI_UF2_UPLOAD_FAMILY) --deploy "$<" > /dev/null \
+		&& echo "$(CPU_DEVICE_PORT)" > "$(BUILD_DIR)/.last_esp32_port" && touch "$@"
+	    #$(PROCESS_OUTPUT)
+endif
+upload_rpi: $(BUILD_DIR)/$(CPU_TARGET)-$(CPU).uf2.upload_rpi.timestamp | silent
+	@
+
+.PRECIOUS: %.uf2
+.PHONY: upload_rpi
+
