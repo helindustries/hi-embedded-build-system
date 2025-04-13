0bb004320fae4d352020472987230d8b58ac72be
Paul Schulze <pschulze@helindustries.com>
2025-04-13 16:56:33 +0200

    Fix builds not properly honouring in-variables

--- SubTargets.mk
+++ SubTargets.mk
@@ -3,12 +3,12 @@ $(SUBDIRS):
 ifeq ($(strip $(VERBOSE)),1)
 	@$(VMSG) "Building Subtarget $(patsubst %/,%,$@)"
 	@$(VCFGMSG) 'NAME:' '$(patsubst %/,%,$@)'
-	@$(VCFGMSG) 'BUILD_DIR:' '$(BUILD_DIR)'
+	@$(VCFGMSG) 'IN_BUILD_DIR:' '$(IN_BUILD_DIR)'
 	@$(VCFGMSG) 'MAKEFLAGS:' '$(MAKEFLAGS)'
 	@$(VCFGMSG) 'MAKECMDGOALS:' '$(MAKECMDGOALS)'
 else
 	@$(MSG) "[MAKE]" "$(patsubst %/,%,$@)"
 endif
-	$(V)$(MAKE) --directory='$(abspath $@)' --file='$(abspath $@)/Makefile' $(MAKEFLAGS) 'BUILD_DIR=$(BUILD_DIR)/$(patsubst %/,%,$@)' $(MAKECMDGOALS)
+	$(V)$(MAKE) --directory='$(abspath $@)' --file='$(abspath $@)/Makefile' $(MAKEFLAGS) 'IN_BUILD_DIR=$(IN_BUILD_DIR)/$(patsubst %/,%,$@)' $(MAKECMDGOALS)
 
 .PHONY: $(TOPTARGETS) $(SUBDIRS)
