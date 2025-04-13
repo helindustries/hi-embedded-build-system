7c314eed65f235cb8cf6a007c6f217140444074b
Paul Schulze <pschulze@helindustries.com>
2025-04-13 17:40:39 +0200

    Clean up formatting

--- BuildSystem.mk
+++ BuildSystem.mk
@@ -1,10 +1,10 @@
-MAKE_BASE_PATH := $(patsubst %/,%,$(dir $(abspath $(firstword $(MAKEFILE_LIST)))))
+ MAKE_BASE_PATH := $(patsubst %/,%,$(dir $(abspath $(firstword $(MAKEFILE_LIST)))))
 MAKE_INC_PATH := $(patsubst %/,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
 STARTUP_MAKEFILE := $(abspath $(firstword $(MAKEFILE_LIST)))
 include $(MAKE_INC_PATH)/Common.mk
 
 ifneq ($(strip $(shell ls --color=never "$(MAKE_INC_PATH)/Config.mk" 2>/dev/null)),)
-include $(MAKE_INC_PATH)/Config.mk
+    include $(MAKE_INC_PATH)/Config.mk
 endif
 
 include $(MAKE_INC_PATH)/Toolchain.mk
