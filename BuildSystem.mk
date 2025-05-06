6dfc6093011d95bb96b223c9c89d348cc10f4b79
Paul Schulze <pschulze@helindustries.com>
2025-05-06 21:01:49 +0200

    Fix the order of including PlatformUtils before Common now

--- BuildSystem.mk
+++ BuildSystem.mk
@@ -1,8 +1,8 @@
 MAKE_BASE_PATH := $(patsubst %/,%,$(dir $(abspath $(firstword $(MAKEFILE_LIST)))))
 MAKE_INC_PATH := $(patsubst %/,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
 STARTUP_MAKEFILE := $(abspath $(firstword $(MAKEFILE_LIST)))
-include $(MAKE_INC_PATH)/Common.mk
 include $(MAKE_INC_PATH)/PlatformUtils/PlatformUtils.mk
+include $(MAKE_INC_PATH)/Common.mk
 
 ifneq ($(call exists,"$(MAKE_INC_PATH)/Config.mk"),)
     include $(MAKE_INC_PATH)/Config.mk
