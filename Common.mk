dab698b0f01f6a3b1f7b83a9c30ac23d9edd686b
Paul Schulze <pschulze@helindustries.com>
2025-05-06 21:01:23 +0200

    Change Common to now properly initialize Python based on it being defined already and the PlatformUtils which command

--- Common.mk
+++ Common.mk
@@ -19,24 +19,26 @@ endif
 CPU_BASE_TARGET ?= $(CPU_TARGET)
 HAS_UPLOAD_TARGET := $(filter upload upload-% %-upload %upload% install install-% %-install %install%,$(MAKECMDGOALS))
 
-ifeq ($(OS),Windows_NT)
-	WHICH ?= where
+# Python is an essential part of this build system, figure things out here!
+ifeq ($(strip $(PYTHON_ADDITIONAL_PATHS)),)
+	# In this case we can just optionally set the variable, PYTHONPATH is exported
+    PYTHON ?= $(shell $(WHICH) python)
 else
-	WHICH ?= which
-endif
+    PYTHON_PATH := $(strip $(call shell-list,$(PYTHON_ADDITIONAL_PATHS)))
 
-
-ifneq ($(strip $(PYTHON_ADDITIONAL_PATHS)),)
-    #PYTHON_PATH := $(strip $(call shell-list,$(PYTHON_ADDITIONAL_PATHS)))
-
-    ifeq ($(strip $(shell echo $$PYTHONPATH)),)
+    ifeq ($(strip $(PYTHONPATH)),)
         PYTHON_ENV += PYTHONPATH="$(PYTHON_PATH)"
     else
-        PYTHON_ENV += PYTHONPATH="$(shell echo $$PYTHONPATH):$(PYTHON_PATH)"
+        PYTHON_ENV += PYTHONPATH="$(PYTHONPATH):$(PYTHON_PATH)"
+    endif
+
+    ifeq ($(strip $(PYTHON)),)
+	    # Further up we use the version of Python in PATH, here we get the specific path
+	    PYTHON := $(PYTHON_ENV) "$(shell $(WHICH) python)"
+    else
+	    PYTHON := $(PYTHON_ENV) "$(PYTHON)"
     endif
 endif
-
-PYTHON := $(PYTHON_ENV) "$(shell $(WHICH) python)"
 
 silent:
 	@:
